ecdd0886aee2d30392299bbc3a05b88ad113b538
Bomin-Lee <pairagon0@gmail.com>
2016-10-30 01:09:47 +0900

    Get clicked button's values.
    To calculate the numbers, get the clicked button's value.
    And made the function of decimal point and cancel button.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,50 @@
 /**
  * Created by diana on 16. 10. 28.
  */
+var result = '';
+
+var calculate = function () {
+	var buttons = document.getElementsByTagName('button');
+	var num = '';
+	var operator;
+
+	for (var i = 0; i < buttons.length; i++) {
+
+		buttons[i].onclick = function () {
+
+			var val = this.innerHTML;
+			if ((/\d/).test(val)) {
+				num += val;
+				result = num;
+				console.log('num : ' + num);
+			} else if (val === '.') {
+				if (num.indexOf('.') === -1) {
+					num += val;
+					result = num;
+				}
+				console.log('. : ' + val);
+			} else if (val === 'c') {
+				num = num.slice(0, num.length - 1);
+				result = num;
+				console.log('c : ' + val);
+			} else if (val === '=') {
+				console.log('= : ' + num);
+			} else {
+				if (val === '%') {
+					console.log(val);
+				} else if (val === 'X') {
+					console.log(val);
+				} else if (val === '-') {
+					console.log(val);
+				} else {
+					console.log(val);
+				}
+			}
+
+		}
+
+	}
+
+
+
+};
