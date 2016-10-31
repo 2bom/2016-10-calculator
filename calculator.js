9d6660e1d577e3b37c642cb7b369562d5f3398f6
Bomin-Lee <pairagon0@gmail.com>
2016-10-31 18:06:10 +0900

    Multiple calculate bug fixed.
    Problem solved by not using inner function calculator.

--- calculator.js
+++ calculator.js
@@ -3,62 +3,64 @@
  */
 
 var calculate = function () {
-    var buttons = document.getElementsByTagName("button");
-    
-    var resultWindow = document.getElementById("result");
+	var buttons = document.getElementsByTagName("button");
+
+	var resultWindow = document.getElementById("result");
 	var num = '0';
-    var numForOperating = '';
+	var numForOperating = '';
 	var operator = '';
 	var errorMessage = 'ERROR';
 
-    var result = '';
+	var result = '';
 
-	var calculator = function (numForOperating, num, operator, val, errorMessage, result){
-		if(result){
-				console.log(operator);
-				console.log(Boolean(operator));
-			if(operator){
-				if (operator === '/'){
-					if (num === '0'){
-						resultWindow.innerHTML = errorMessage;
-					} else {
-						result = parseFloat(numForOperating) / parseFloat(num);
-
-					}
-				} else if (operator === 'x'){
-					result = parseFloat(numForOperating) * parseFloat(num);
-
-				} else if (operator === '-'){
-					result = parseFloat(numForOperating) - parseFloat(num);
-
-				} else {
-					result = parseFloat(numForOperating) + parseFloat(num);
-
-				}
-			} else {
-				result = num;
-			}
-
-			resultWindow.innerHTML = result;
-		} else {
-			if (operator === '/'){
-				if (num === '0'){
-					resultWindow.innerHTML = errorMessage;
-				} else {
-					numForOperating = parseFloat(numForOperating) / parseFloat(num);
-				}
-			} else if (operator === 'x'){
-				numForOperating = parseFloat(numForOperating) * parseFloat(num);
-			} else if (operator === '-'){
-				numForOperating = parseFloat(numForOperating) - parseFloat(num);
-			} else {
-				numForOperating = parseFloat(numForOperating) + parseFloat(num);
-			}
-
-			resultWindow.innerHTML = numForOperating;
-		}
-	};
-
+	// var calculator = function (numForOperating, num, operator, val, errorMessage, result){
+	// 	if(result){
+	// 			// console.log(operator);
+	// 			// console.log(Boolean(operator));
+	// 		if(operator){
+	// 			if (operator === '/'){
+	// 				if (num === '0'){
+	// 					resultWindow.innerHTML = errorMessage;
+	// 				} else {
+	// 					result = parseFloat(numForOperating) / parseFloat(num);
+	//
+	// 				}
+	// 			} else if (operator === 'x'){
+	// 				result = parseFloat(numForOperating) * parseFloat(num);
+	//
+	// 			} else if (operator === '-'){
+	// 				result = parseFloat(numForOperating) - parseFloat(num);
+	//
+	// 			} else {
+	// 				result = parseFloat(numForOperating) + parseFloat(num);
+	//
+	// 			}
+	// 		} else {
+	// 			result = num;
+	// 		}
+	//
+	// 		resultWindow.innerHTML = result;
+	// 	} else {
+	// 		console.log('num : ' + num);
+	// 		console.log('numFO : ' + numForOperating);
+	// 		if (operator === '/'){
+	// 			if (num === '0'){
+	// 				resultWindow.innerHTML = errorMessage;
+	// 			} else {
+	// 				numForOperating = parseFloat(numForOperating) / parseFloat(num);
+	// 			}
+	// 		} else if (operator === 'x'){
+	// 			numForOperating = parseFloat(numForOperating) * parseFloat(num);
+	// 		} else if (operator === '-'){
+	// 			numForOperating = parseFloat(numForOperating) - parseFloat(num);
+	// 		} else {
+	// 			numForOperating = parseFloat(numForOperating) + parseFloat(num);
+	// 		}
+	//
+	// 		resultWindow.innerHTML = numForOperating;
+	// 	}
+	// };
+	//
 
 	for (var i = 0; i < buttons.length; i++) {
 
@@ -67,9 +69,7 @@ var calculate = function () {
 			var val = this.innerHTML;
 
 			if ((/\d/).test(val)) {
-                // console.log('operator : ' + operator);
-                console.log(num);
-                // console.log(val.constructor);
+				console.log(num);
 
 				num = (num === '0' ? val : num + val);
 
@@ -83,38 +83,61 @@ var calculate = function () {
 
 				resultWindow.innerHTML = num;
 
-				// console.log('. : ' + num);
 			} else if (val === 'c') {
 				num = num.slice(0, num.length - 1);
-                if(num === ''){
-                    num = '0';
-                }
-
-				resultWindow.innerHTML = num;
-				// console.log(num.constructor);
-			} else if (val !== '=') {
-				if (!operator){
-					numForOperating = num;
-					// console.log('operator none');
-				} else {
-					calculator(numForOperating, num, operator, val, errorMessage);
+				if(num === ''){
+					num = '0';
 				}
 
+				resultWindow.innerHTML = num;
+			} else if (val !== '=') {
+
+				if (!operator){
+					numForOperating = num;
+				} else {
+					if (operator === '/'){
+						if (num === '0'){
+							resultWindow.innerHTML = errorMessage;
+						} else {
+							numForOperating = parseFloat(numForOperating) / parseFloat(num);
+						}
+					} else if (operator === 'x'){
+						numForOperating = parseFloat(numForOperating) * parseFloat(num);
+					} else if (operator === '-'){
+						numForOperating = parseFloat(numForOperating) - parseFloat(num);
+					} else {
+						numForOperating = parseFloat(numForOperating) + parseFloat(num);
+					}
+				}
+
+				resultWindow.innerHTML = numForOperating;
 				num = '0';
 				operator = val;
-				// console.log(operator);
-			} else {
-				calculator(numForOperating, num, operator, val, errorMessage, true);
 
+			} else {
+				if(operator){
+					if (operator === '/'){
+						if (num === '0'){
+							resultWindow.innerHTML = errorMessage;
+						} else {
+							result = parseFloat(numForOperating) / parseFloat(num);
+						}
+					} else if (operator === 'x'){
+						result = parseFloat(numForOperating) * parseFloat(num);
+					} else if (operator === '-'){
+						result = parseFloat(numForOperating) - parseFloat(num);
+					} else {
+						result = parseFloat(numForOperating) + parseFloat(num);
+					}
+				} else {
+					result = num;
+				}
+
+				resultWindow.innerHTML = result;
 				numForOperating ='';
 				num = '0';
 				operator = '';
-				// console.log('= : ' + num);
 			}
-
-            // console.log(resultWindow.innerHTML);
-            // console.log(num);
-
 
 		}
 
